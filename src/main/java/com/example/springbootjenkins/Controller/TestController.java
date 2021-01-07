205b150fb8fe8e0adf12bd67b3035d4217a702e0
panqigeng <panqigenglove@163.com>
2021-01-07 23:42:10 +0800

    dddd

--- src/main/java/com/example/springbootjenkins/Controller/TestController.java
+++ src/main/java/com/example/springbootjenkins/Controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ResponseBody
 public class TestController {
 
-//bbbbbbbbbbbbbbbbbbb
+//ccccccccccccccccccccccccccccc
     @RequestMapping("/test")
     public String getHello() {
 //        return "hello world jenkins!";
